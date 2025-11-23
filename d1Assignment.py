343e350bfca2eb1f3e4beab4196c1341e8f156ec
Scott <sjb8108@rit.edu>
2025-11-23 18:26:32 -0500

    simple function to inject a directory of jsons to aws firehose

--- d1Assignment.py
+++ d1Assignment.py
@@ -1,6 +1,17 @@
 import json
 import boto3
+import key
+import os
+import time
 
 def main():
-    print()
+    client = boto3.client("kinesis", aws_access_key_id=key.AWS_SESSION, aws_secret_access_key=key.AWS_KEY, region_name='us-east-2')
+    directoryOfJSONS = r"C:\\Users\\Scott\\Documents\\data_engineering\\InjectedFiles"
+    for filename in os.listdir(directoryOfJSONS):
+        with open(os.path.join(directoryOfJSONS, filename)) as f:
+            data = json.load(f)
+        response = client.put_record(StreamName="D1-Assignment-Megazone", Data=json.dumps(data), PartitionKey="1")
+        print(response)
     
+if __name__ == "__main__":
+    main()
