407b43ead7f6274e13d4151dfcec4d13b607e74b
Scott <sjb8108@rit.edu>
2025-11-24 17:29:52 -0500

    added comments

--- d0FullCompletedAssignment/d0assignment.py
+++ d0FullCompletedAssignment/d0assignment.py
@@ -2,18 +2,24 @@ import json
 import os
 
 """
-Given CSV files with specific columns, cleans the data in the CSV file so that the output file contains
-member id, full name, and paid amount. The output file will be a seperate JSON file
+Overall Program Summary:
 
-Also reports the highest paid amount with the members info
-Parameters: 
-memberInfo : string
-    CSV file as a string containing member info
-memberPaidInfo : string
-    CSV file as a string containing paid info
+Given CSV files with specific columns, cleans the data in the CSV file so that the output file contains
+member id, full name, and paid amount. The output file will be a seperate JSON file. Also prints out 
+member with highest paid amount and the total amount paided.
 
 """
 
+
+"""
+Summary:
+    Cleans the data in CSV by removing entries that have no name connected to the member id
+Parameters:
+    directory: a string that is the directory the current python file is located in
+    memberInfoFile: the name of the csv we are cleaning
+Return:
+    list->dict: This is a list of dictonaries that contains all valid member info
+"""
 def getDataFromMemberInfo(directory, memberInfoFile) -> list[dict]:
     memberInfoList = []
     memberInfoFile = os.path.join(directory, "CSV Files", memberInfoFile)
@@ -24,24 +30,36 @@ def getDataFromMemberInfo(directory, memberInfoFile) -> list[dict]:
             seperatedData = entry.split(",")
             memberInfoDict["memberId"] = seperatedData[0] #seperatedData[0] is the member id in the csv file
             seperatedData[2] = seperatedData[2].replace("\n", "") #removes \n in the string
-            if seperatedData[1] == "":
+            if seperatedData[1] == "": #if hole in data continue
                 continue
-            else:
+            else: #else we put in the first and last name of the person
                 memberInfoDict["firstName"] = seperatedData[1]
                 memberInfoDict["lastName"] = seperatedData[2]
             memberInfoList.append(memberInfoDict)
     return memberInfoList
 
+"""
+Summary:
+    Merges the data in the member paid info csv to the member info list. Making sure to avoid
+    any name conflictions and that the member exists in the cleaned list of members
+Parameters:
+    directory: a string that is the directory the current python file is located in
+    memberPaidInfoFile: the name of the csv file that contain the paid info for members
+    memberInfoList: This is a list of dictonaries that contains all valid member info
+Return:
+    list->dict: This is a list of dictonaries that contains all valid member info along with 
+    the paid amount
+"""
 def mergeDataFromMemberPaidInfo(directory, memberPaidInfoFile, memberInfoList) -> list[dict]:
     memberPaidInfoFile = os.path.join(directory, "CSV Files", memberPaidInfoFile)
     with open(memberPaidInfoFile, "r") as file:
         next(file) #skips first line
-        index = 0
-        for entry in file:
+        index = 0 #index for memberInfoList to keep track of what paidAmount goes to what person
+        for entry in file: #looping over all lines in csv
             seperatedData = entry.split(",")
             memberId = seperatedData[0]
             memberExists = False
-            for memberInfoEntry in memberInfoList:
+            for memberInfoEntry in memberInfoList: #looping over memberInfo list to see if member exitsts
                 if memberInfoEntry["memberId"] == memberId:
                     if seperatedData[1] == "":
                         memberExists = True
@@ -51,7 +69,7 @@ def mergeDataFromMemberPaidInfo(directory, memberPaidInfoFile, memberInfoList) -
                         lastName = fullNameList[1]
                         #checking if name conflict
                         if firstName != memberInfoList[index]["firstName"] or lastName != memberInfoList[index]["lastName"]:
-                            memberInfoList.remove(memberInfoEntry)
+                            memberInfoList.remove(memberInfoEntry) #removes entry if name conflict
                             memberExists = False
                         else:
                             memberExists = True
@@ -61,8 +79,17 @@ def mergeDataFromMemberPaidInfo(directory, memberPaidInfoFile, memberInfoList) -
                 index+=1
     return memberInfoList
 
+"""
+Summary:
+    Prints out the statistics related the member info and paid info. Prints out the
+    member who has the highest paid amount. Also prints out the total paid amount by
+    all valid members
+Parameters:
+    highestPaidList: a sorted list of dictonaries by the value of paidedAmount in the
+    dictonaries. The dictonaries contain member info and paid amount info
+"""
 def printStats(highestPaidList) -> None:   
-    highestPaidPerson = highestPaidList[0]
+    highestPaidPerson = highestPaidList[0] 
     print("Person with the highest paid amount:")
     for key in highestPaidPerson:
         print("\t" + str(key) + " : " + str(highestPaidPerson[key]))
@@ -70,19 +97,33 @@ def printStats(highestPaidList) -> None:
     for person in highestPaidList:
         totalPaidAmount += int(person["paidAmount"])
     print("Total Paided Amount: " + str(totalPaidAmount))
-    
+
+"""
+Summary:
+    Saves all valid member info and paid info into a JSON file for more data analysis
+Parameters:
+    memberInfoAndPaidInfoList: This is a list of dictonaries that contains all valid member info along with 
+    the paid amount
+"""
 def outputCleanedData(memberInfoAndPaidInfoList) -> None:
     with open("d0FullCompletedAssignment\\cleanedMemberInfoAndPaidInfo.json", "w") as file:
         json.dump(memberInfoAndPaidInfoList, file, indent=2)
-    
+
+"""
+Summary:
+    Calls all functions performing the data cleaning, printing statistics, and saving output
+Parameters:
+    memberInfoFile: the name of the csv file that contain the member info
+    memberPaidInfoFile: the name of the csv file that contain the paid info
+"""
 def main(memberInfoFile, memberPaidInfoFile):
     directory = os.path.dirname(os.path.abspath(__file__))
     memberInfoList = getDataFromMemberInfo(directory, memberInfoFile)
     memberInfoAndPaidInfoList = mergeDataFromMemberPaidInfo(directory, memberPaidInfoFile, memberInfoList)
+    #sorts list by paidAmount value located in each dictonary, highest paid amount at index 0
     highestPaidList = sorted(memberInfoAndPaidInfoList, key=lambda d: d["paidAmount"], reverse=True)
     printStats(highestPaidList)
     outputCleanedData(memberInfoAndPaidInfoList)
-    
     
 if __name__ == "__main__":
     main("memberInfo.csv", "memberPaidInfo.csv")
